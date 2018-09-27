8002c557770ddbc891d0b6d80650e553cfad72b8
Gabin An <agb94@kaist.ac.kr>
2018-09-27 13:39:27 +0900

    create Feature class

--- predictor.py
+++ predictor.py
@@ -1,47 +1,42 @@
 import os
 import argparse
 
-MODEL = "div(MBS, neg(mul(HD, div(LS, MBS))))"
-FEATURE_NAMES = ['LS', 'HD', 'MBS']
-"""
-- MBS: The Sum of Matching Blocks' Size
-- HD: Hamming Distance
-- LS: Lexical Similarity
-"""
-
-def compute_score(*args):
-    from math import sin, cos
-    from operator import add, sub, mul, neg
+class Feature:
+    """
+    * SCORING MODEL: div(MBS, neg(mul(HD, div(LS, MBS))))
     
-    def protectedDiv(left, right):
-        try:
-            return left / right
-        except ZeroDivisionError:
-            return 1
-
-    div = lambda a, b: protectedDiv(a, b)
+    - MBS: The Sum of Matching Blocks' Size
+    - HD: Hamming Distance
+    - LS: Lexical Similarity
+    """
+    def __init__(self, ls, hd, mbs):
+        self.ls = float(ls)
+        self.hd = float(hd)
+        self.mbs = float(mbs)
     
-    assert len(args) == len(FEATURE_NAMES)
-    model = MODEL
-    for i, feature_name in enumerate(FEATURE_NAMES):
-        model = model.replace(feature_name, 'args[{}]'.format(i))
-    return eval(model)
+    def get_score(self):
+        from math import sin, cos
+        from operator import add, sub, mul, neg
+        div = lambda a, b: a / b if b != 0 else 1 # protected division operator
+        return div(self.mbs, neg(mul(self.hd, div(self.ls, self.mbs))))
 
 def extract_unit(ingredient, source):
     import difflib
+
     features = list()
     max_mbs = 0
+
     for line in source:
         seq_matcher = difflib.SequenceMatcher(lambda x: x == " ", ingredient, line)
         ls = 1 - seq_matcher.ratio()
         hd = len(list(filter(lambda op: op[0] != 'equal', seq_matcher.get_opcodes())))
         mbs = sum(map(lambda b: b.size, seq_matcher.get_matching_blocks()))
         max_mbs = max(mbs, max_mbs)
-        features.append([ls, hd, mbs])
+        features.append(Feature(ls, hd, mbs))
 
-    for row in features:
+    for feature in features:
         # Normalization
-        row[-1] /= float(max_mbs)
+        feature.mbs /= float(max_mbs)
     
     return features
 
@@ -52,16 +47,16 @@ def predict_unit(ingredient: str, source: list, verbose=False):
     features = extract_unit(ingredient, source)
     if verbose:
         # verbose mode
-        values = [compute_score(*row) for row in features]
+        values = [feature.get_score() for feature in features]
         print("===========================================")
         print(ingredient)
         for i, lineno in enumerate(sorted(range(len(values)), key=lambda k: values[k])[:20]):
             print("{}\t{:.6f}\t{:>5}:\t{}".format(i+1, values[lineno], lineno + 1, source[lineno]))
 
-    min_value = compute_score(*features[0])
+    min_value = features[0].get_score()
     candidates = list()
-    for i, row in enumerate(features):
-        score = compute_score(*row)
+    for i, feature in enumerate(features):
+        score = feature.get_score()
         if score > min_value:
             pass
         elif score < min_value:
