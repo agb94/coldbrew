e459344629dffa6a94eea05b18eaadb14fb6e63f
Gabin An <agb94@kaist.ac.kr>
2018-10-03 19:25:42 +0900

    stable

--- predictor.py
+++ predictor.py
@@ -1,10 +1,12 @@
 import os
 import argparse
 from pygments.lexers import get_lexer_by_name
-from pygments.token import string_to_tokentype
+from pygments.token import Keyword, Text
 
 JL = get_lexer_by_name('java')
-TOKENTYPE_KEYWORD = string_to_tokentype('Token.Keyword')
+# See java-keywords-stat.txt
+SELECTED_KEYWORDS = ['try', 'case', 'enum', 'switch', 'catch', 'package', 'import', 'class', 'for', 'interface',
+        'void', 'else', 'static', 'return', 'if', 'long', 'while', 'native']
 
 class Feature:
     """
@@ -20,11 +22,14 @@ class Feature:
         self.mbs = float(mbs)
         self.ks = float(ks)
     
+    def __str__(self):
+        return "{:6.3f}l {:6.3f}h {:6.3f}m {:6.3f}k".format(self.ls, self.hd, self.mbs, self.ks)
+
     def get_score(self):
         from math import sin, cos
         from operator import add, sub, mul, neg
         div = lambda a, b: a / b if b != 0 else 1 # protected division operator
-        return div(self.mbs, neg(mul(self.hd, div(self.ls, self.mbs))))
+        return add(div(self.mbs, neg(mul(self.hd, div(self.ls, self.mbs)))), self.ks)
 
 def extract_unit(ingredient, source):
     import difflib
@@ -34,17 +39,15 @@ def extract_unit(ingredient, source):
 
     keywords = []
     for token in JL.get_tokens(ingredient):
-        if token[0] == TOKENTYPE_KEYWORD:
+        if token[0] in Keyword and token[1] in SELECTED_KEYWORDS:
             keywords.append(token[1])
 
     for line in source:
-
         line_keywords = []
         for token in JL.get_tokens(line):
-            if token[0] == TOKENTYPE_KEYWORD:
+            if token[0] in Keyword and token[1] in SELECTED_KEYWORDS:
                 line_keywords.append(token[1])
         ks = 1 - difflib.SequenceMatcher(None,keywords,line_keywords).ratio()
-
         seq_matcher = difflib.SequenceMatcher(lambda x: x == " ", ingredient, line)
         ls = 1 - seq_matcher.ratio()
         hd = len(list(filter(lambda op: op[0] != 'equal', seq_matcher.get_opcodes())))
@@ -69,7 +72,7 @@ def predict_unit(ingredient: str, source: list, verbose=False):
         print("===========================================")
         print(ingredient)
         for i, lineno in enumerate(sorted(range(len(values)), key=lambda k: values[k])[:20]):
-            print("{}\t{:.6f}\t{:>5}:\t{}".format(i+1, values[lineno], lineno + 1, source[lineno]))
+            print("{}\t{:.6f}\t{}\t{:>5}:\t{}".format(i+1, values[lineno], features[lineno], lineno + 1, source[lineno]))
 
     min_value = features[0].get_score()
     candidates = list()
@@ -83,10 +86,6 @@ def predict_unit(ingredient: str, source: list, verbose=False):
         else:
             candidates.append(i)
 
-    # 1. Return the middle one if many candidates exist
-    # return candidates[len(candidates)//2] + 1
-
-    # 2. Return the last one if many candidates exist
     return candidates[-1] + 1
 
 def predict(path, verbose=False):
